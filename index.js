fe459ac0d8e3065d4c1670951f69b045e29ecf9f
‘Juliano <julianoperins@gmail.com>
2020-12-23 16:36:12 -0500

    set up db, express and routes

--- index.js
+++ index.js
@@ -3,16 +3,32 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+//! setup mongoose
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_STRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB Connection established");
+  }
+);
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+//! Include route as middleware
+app.use("/users", require("./routes/userRouter"));
 
 app.get("/", (req, res) => {
   res.send("Hi there ");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT || 3000, () => {
-  console.log(`Server running on 3000 ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
---
+++ models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 5,
+  },
+  displayedName: {
+    type: String,
+  },
+});
+
+module.exports = User = mongoose.model("user", userSchema);
