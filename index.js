d397ea21c6c25c3c81d40279f3f733c8f3ae1b4f
‘Juliano <julianoperins@gmail.com>
2020-12-24 17:51:10 -0500

    Worked on the header style

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
   res.send("Hi there ");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
