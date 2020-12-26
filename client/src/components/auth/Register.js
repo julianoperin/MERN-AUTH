24a9a6c5f0b95445ec9e554583f9c1f66eab9910
‘Juliano <julianoperins@gmail.com>
2020-12-26 15:24:12 -0500

    workeed on the registar form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,8 +2,21 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div>
+    <div className="page">
       <h1>Register</h1>
+      <form>
+        <label htmlFor="register-email">Email</label>
+        <input type="text" id="register-email" />
+
+        <label htmlFor="register-password">Password</label>
+        <input type="text" id="register-password" />
+        <input type="text" id="register-password" />
+
+        <label htmlFor="register-display-name">Display Name</label>
+        <input type="text" id="register-display-name" />
+
+        <input type="submit" value="Register" />
+      </form>
     </div>
   );
 };
