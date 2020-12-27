4ab04186815231c8a24740b32c88a60e65b1fbe8
‘Juliano <julianoperins@gmail.com>
2020-12-26 20:28:31 -0500

    Handled all errors

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,37 +2,46 @@ import React, { useState, useContext } from "react";
 import Axios from "axios";
 import UserContext from "../../context/UserContext";
 import { useHistory } from "react-router-dom";
+import ErrorNotice from "../misc/ErrorNotice";
 
 const Login = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [error, setError] = useState();
 
   const { setUserData } = useContext(UserContext);
   const history = useHistory();
 
   const submit = async (e) => {
-    e.preventDefault();
-    const loginUser = { email, password };
-    console.log(loginUser);
+    try {
+      e.preventDefault();
+      const loginUser = { email, password };
+      console.log(loginUser);
 
-    //! Login
-    const loginRes = await Axios.post(
-      "http://localhost:5000/users/login",
-      loginUser
-    );
+      //! Login
+      const loginRes = await Axios.post(
+        "http://localhost:5000/users/login",
+        loginUser
+      );
 
-    //! Get the response data
-    setUserData({
-      token: loginRes.data.token,
-      user: loginRes.data.user,
-    });
-    localStorage.setItem("auth-token", loginRes.data.token);
-    history.push("/");
+      //! Get the response data
+      setUserData({
+        token: loginRes.data.token,
+        user: loginRes.data.user,
+      });
+      localStorage.setItem("auth-token", loginRes.data.token);
+      history.push("/");
+    } catch (err) {
+      err.response.data.msg && setError(err.response.data.msg);
+    }
   };
 
   return (
     <div className="page">
       <form className="form">
+        {error && (
+          <ErrorNotice message={error} clearError={() => setError(undefined)} />
+        )}
         <h1>Login</h1>
 
         <label htmlFor="login-email">Email</label>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,38 +2,47 @@ import React, { useState, useContext } from "react";
 import Axios from "axios";
 import UserContext from "../../context/UserContext";
 import { useHistory } from "react-router-dom";
+import ErrorNotice from "../misc/ErrorNotice";
 
 const Register = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [passwordCheck, setPasswordCheck] = useState();
   const [displayedName, setDisplayedName] = useState();
+  const [error, setError] = useState();
 
   const { setUserData } = useContext(UserContext);
   const history = useHistory();
 
   const submit = async (e) => {
     e.preventDefault();
-    const newUser = { email, password, passwordCheck, displayedName };
-    //! Register
-    await Axios.post("http://localhost:5000/users/register", newUser);
-    //! Login
-    const loginRes = await Axios.post("http://localhost:5000/users/login", {
-      email,
-      password,
-    });
-    //! Get the response data
-    setUserData({
-      token: loginRes.data.token,
-      user: loginRes.data.user,
-    });
-    localStorage.setItem("auth-token", loginRes.data.token);
-    history.push("/");
+    try {
+      const newUser = { email, password, passwordCheck, displayedName };
+      //! Register
+      await Axios.post("http://localhost:5000/users/register", newUser);
+      //! Login
+      const loginRes = await Axios.post("http://localhost:5000/users/login", {
+        email,
+        password,
+      });
+      //! Get the response data
+      setUserData({
+        token: loginRes.data.token,
+        user: loginRes.data.user,
+      });
+      localStorage.setItem("auth-token", loginRes.data.token);
+      history.push("/");
+    } catch (err) {
+      err.response.data.msg && setError(err.response.data.msg);
+    }
   };
 
   return (
     <div className="page">
       <form className="form">
+        {error && (
+          <ErrorNotice message={error} clearError={() => setError(undefined)} />
+        )}
         <h1>Register</h1>
 
         <label htmlFor="register-email">Email</label>
